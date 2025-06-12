cb22d7bb1452d415a00a746eaa9b76290c423af3
FAWWAZ <fawwaznaufaldzulfikar@gmail.com>
2025-06-12 15:37:26 +0700

    membuat if +while

--- Queue.cpp
+++ Queue.cpp
@@ -95,3 +95,18 @@ public:
                 cout << queue_array[FRONT_position] << " ";
                 FRONT_position++;
             }
+
+            FRONT_position = 0;
+            if (FRONT_position <= REAR_position)
+        {
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+        }
+
+    }
+};
