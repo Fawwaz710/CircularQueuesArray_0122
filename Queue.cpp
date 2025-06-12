c09c542024e24c283ff4f853d96014e6b4add3d2
FAWWAZ <fawwaznaufaldzulfikar@gmail.com>
2025-06-12 15:36:45 +0700

    membuat else

--- Queue.cpp
+++ Queue.cpp
@@ -87,4 +87,11 @@ public:
             }
             cout << endl;
         }
-       
+        else
+        {
+            //
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
